5b0353a9084418187a2e18e87462490298a54d69
vinaykumarsasumana <sasumana.kumar@tavant.com>
2021-12-14 22:13:37 +0530

    Commit after modication in BuyerLogincntl

--- CoreWebApiJWT/CoreWebApiJWT/Controllers/BuyerLoginController.cs
+++ CoreWebApiJWT/CoreWebApiJWT/Controllers/BuyerLoginController.cs
@@ -15,29 +15,57 @@ namespace CoreWebApiJWT.Controllers
     {
         DemoTokenContexts DB = new DemoTokenContexts();
 
+        //[Route("BuyerLogin")]
+        //[HttpPost]
+        //public Response BuyerLogin(string Email, string Password)
+        //{
+        //    //var log = DB.SellerRegisters.Where(x => x.EmailId.Equals(reg.EmailID)).FirstOrDefault();
+        //    //var log = DB.SellerRegisters.Where(x => x.Id.Equals(loginId) ).FirstOrDefault();
+        //    var log = DB.BuyerRegistrations.Where(x => x.EmailId.Equals(Email)).FirstOrDefault();
+        //    var pass = DB.BuyerRegistrations.Where(x => x.BuyerPassword.Equals(Password)).FirstOrDefault();
+        //    if (log == null || pass == null)
+        //    {
+        //        return new Response { Status = "Invalid", Message = "Invalid User." };
+        //    }
+        //    else
+        //    {
+        //        BuyerLogin EL = new BuyerLogin();
+        //        //EL.SellerId = RegID;
+
+        //        EL.EmailId = Email;
+        //        EL.BuyerPassword = Password;
+        //        DB.BuyerLogins.Add(EL);
+        //        DB.SaveChanges();
+        //        return new Response { Status = "Success", Message = "Login Successful" };
+        //    }
+
+
         [Route("BuyerLogin")]
         [HttpPost]
-        public Response BuyerLogin(string Email, string Password)
+        public Response BuyerLogin(LoginModel loginDetails)
         {
-            //var log = DB.SellerRegisters.Where(x => x.EmailId.Equals(reg.EmailID)).FirstOrDefault();
-            //var log = DB.SellerRegisters.Where(x => x.Id.Equals(loginId) ).FirstOrDefault();
-            var log = DB.BuyerRegistrations.Where(x => x.EmailId.Equals(Email)).FirstOrDefault();
-            var pass = DB.BuyerRegistrations.Where(x => x.BuyerPassword.Equals(Password)).FirstOrDefault();
-            if (log == null || pass == null)
+
+            var log = DB.BuyerRegistrations.Where(x => x.EmailId.Equals(loginDetails.EmailId)).FirstOrDefault();
+            if (log != null)
             {
-                return new Response { Status = "Invalid", Message = "Invalid User." };
+                if (log.BuyerPassword == loginDetails.SellerPassword)
+                {
+                    return new Response { Status = "Success", Message = "Login Successful" };
+                }
+                else
+                {
+                    return new Response { Status = "Invalid", Message = "Invalid User." };
+                }
             }
             else
             {
-                BuyerLogin EL = new BuyerLogin();
-                //EL.SellerId = RegID;
-
-                EL.EmailId = Email;
-                EL.BuyerPassword = Password;
-                DB.BuyerLogins.Add(EL);
-                DB.SaveChanges();
-                return new Response { Status = "Success", Message = "Login Successful" };
+                return new Response { Status = "Invalid", Message = "Invalid User." };
             }
+
+
         }
+
+
     }
+    
 }
