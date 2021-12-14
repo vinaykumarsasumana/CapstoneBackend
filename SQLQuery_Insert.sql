74cea721d1935729e31c0ef5956ffdd875404214
vinaykumarsasumana <sasumana.kumar@tavant.com>
2021-12-14 13:02:51 +0530

    Fisrt Commit on SQLInsert.SQL

---
+++ SQLQuery_Insert.sql
@@ -0,0 +1,103 @@
+use ShoppingCapstoneDB
+
+select * from ProductTable
+
+select * from SellerRegistration
+
+insert into SellerRegistration values ('vini','kumar','vini@gmail.com',
+                          'vini@007','India','9908276025','HYD','viniTavant','vinitavant.com')
+
+insert into SellerRegistration values ('Akash','Patel','akash@gmail.com',
+                          'akash@007','India','1234554321','Bangalore','akashTavant','akashtavant.com')
+
+insert into SellerRegistration values ('sutej','Girish','sutej@gmail.com',
+                          'sutej@007','India','0123456789','HYD','sutejTavant','sutejtavant.com')
+
+insert into SellerRegistration values ('simran','Dodmise','simran@gmail.com',
+                          'simran@007','India','9999988888','Mumbai','simranTavant','simrantavant.com')
+
+insert into SellerRegistration values ('Sid','sid','sid@gmail.com',
+                          'sid@007','India','8888876025','HYD','sidTavant','sidtavant.com')
+
+select * from SellerRegistration
+
+
+
+select * from ProductTable
+--Smart Phone
+
+insert into ProductTable values (2,'Apple','Electronics','Smart Phone','Iphone',100000,500,'This is Apple IPhone','India','Apple IPhone T & C')
+
+
+insert into ProductTable values (2,'Apple','Electronics','Smart Phone','Iphone3',70000,400,'This is Apple IPhone','India','Apple IPhone T & C')
+
+insert into ProductTable values (3,'Nokia','ElectronicGoods','Smart Phone','Nokiaphone',90000,500,'This is Nokia Phone','India','Nokia Phone T & C')
+
+insert into ProductTable values (3,'Nokia','ElectronicGoods','Smart Phone','Nokia 2s',30000,500,'This is Nokia Phone','India','Nokia Phone T & C')
+
+insert into ProductTable values (4,'MI','Electronics','Smart Phone','MI Phone',44000,500,'This is MI Phone','India','MI Phone T & C')
+
+insert into ProductTable values (4,'MI','Electronics','Smart Phone','MI 7s',55000,500,'This is MI Phone','India','MI Phone T & C')
+
+insert into ProductTable values (5,'RealMe','Electronics','Smart Phone','RealMe 8k',8000,500,'This is RealMe Phone','India','RealMe Phone T & C')
+
+insert into ProductTable values (5,'RealMe','Electronics','Smart Phone','RealMe 12 g',33000,500,'This is RealMe Phone','India','RealMe Phone T & C')
+
+select * from ProductTable
+--Smart Watch
+
+insert into ProductTable values (2,'Apple','Electronics','Smart Watch','IWatch',100000,500,'This is Apple IWatch','India','Apple IWatch T & C')
+
+insert into ProductTable values (2,'Apple','Electronics','Smart Watch','IWatch',70000,400,'This is Apple IWatch','India','Apple IWatch T & C')
+
+insert into ProductTable values (3,'Nokia','ElectronicGoods','Smart Watch','NokiaWatch',90000,500,'This is Nokia Watch','India','Nokia Watch T & C')
+
+insert into ProductTable values (3,'Nokia','ElectronicGoods','Smart Watch','Nokia Watch 2s',30000,500,'This is Nokia Watch','India','Nokia Watch T & C')
+
+insert into ProductTable values (4,'MI','Electronics','Smart Watch','MI Watch',44000,500,'This is MI Watch','India','MI Watch T & C')
+
+insert into ProductTable values (4,'MI','Electronics','Smart Watch','MI Watch 7s',55000,500,'This is MI Watch','India','MI Watch T & C')
+
+insert into ProductTable values (5,'RealMe','Electronics','Smart Phone','RealMe Watch',8000,500,'This is RealMe Watch','India','RealMe Watch T & C')
+
+insert into ProductTable values (5,'RealMe','Electronics','Smart Watch','RealMe Watch g',33000,500,'This is RealMe Watch','India','RealMe Watch T & C')
+
+select * from ProductTable
+
+--Camera
+insert into ProductTable values (2,'Apple','Electronics','Camera','ICamera',100000,500,'This is Apple Camera','India','Apple Camera T & C')
+
+insert into ProductTable values (2,'Apple','Electronics','Camera','ICamera',70000,400,'This is Apple Camera','India','Apple Camera T & C')
+
+insert into ProductTable values (3,'Nokia','ElectronicGoods','Camera','NokiaCamera',90000,500,'This is Nokia Camera','India','Nokia Camera T & C')
+
+insert into ProductTable values (3,'Nokia','ElectronicGoods','Camera','Nokia Camera 2s',30000,500,'This is Nokia Camera','India','Nokia Camera T & C')
+
+insert into ProductTable values (4,'MI','Electronics','Camera','MI Camera',44000,500,'This is MI Camera','India','MI Camera T & C')
+
+insert into ProductTable values (4,'MI','Electronics','Camera','MI Camera 7s',55000,500,'This is MI Camera','India','MI Camera T & C')
+
+insert into ProductTable values (5,'RealMe','Electronics','Camera','RealMe Camera',8000,500,'This is RealMe Camera','India','RealMe Camera T & C')
+
+insert into ProductTable values (5,'RealMe','Electronics','Camera','RealMe Camera g',33000,500,'This is RealMe Camera','India','RealMe Camera T & C')
+
+select * from ProductTable
+
+--Shoe
+insert into ProductTable values (2,'Adidas','Goods','Shoe','Adidas Shoe',100000,500,'This is Adidas Shoe','India','Adidas Shoe T & C')
+
+insert into ProductTable values (2,'Adidas','Goods','Shoe','Adidas Shoe',70000,400,'This is Adidas Shoe','India','Adidas Shoe T & C')
+
+insert into ProductTable values (3,'Nike','Goods','Shoe','Nike Shoe',90000,500,'This is Nike Shoe','India','Nike Shoe T & C')
+
+insert into ProductTable values (3,'Nike','Goods','Shoe','Nike Shoe',30000,500,'This is Nike Shoe','India','Nike Shoe T & C')
+
+insert into ProductTable values (4,'Puma','Goods','Shoe','Puma Shoe',44000,500,'This is Puma Shoe','India','Puma Shoe T & C')
+
+insert into ProductTable values (4,'Puma','Goods','Shoe','Puma Shoe os',55000,500,'This is Puma Shoe','India','Puma Shoe T & C')
+
+insert into ProductTable values (5,'Foot Locker','Goods','Shoe','Foot Locker Shoe US',8000,500,'This is Foot Locker Shoe','India','Foot Locker Shoe T & C')
+
+insert into ProductTable values (5,'Foot Locker','Goods','Shoe','Foot Locker Shoe g',33000,500,'This is Foot Locker Shoe','India','Foot Locker Shoe T & C')
+
+select * from ProductTable
