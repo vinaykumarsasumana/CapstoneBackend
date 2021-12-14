224783124f490ff38e71e9b7b31e29f2962d923b
vinaykumarsasumana <sasumana.kumar@tavant.com>
2021-12-15 00:07:48 +0530

    Commit after adding OrderController

---
+++ CoreWebApiJWT/CoreWebApiJWT/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using CoreWebApiJWT.DataContexts;
+using CoreWebApiJWT.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApiJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        DemoTokenContexts DB = new DemoTokenContexts();
+        [Route("OrderTheProduct")]
+        [HttpPost]
+        public object OrderTheProduct(OrderTable Reg)
+        {
+            try
+            {
+                OrderTable EL = new OrderTable();
+
+
+
+                if (EL.OrderId == 0)
+                {
+                    EL.ProductId = Reg.ProductId;
+                    EL.BuyerId = Reg.BuyerId;
+                    EL.SellerId = Reg.SellerId;
+                    EL.ProductName = Reg.ProductName;
+                    EL.ProductPrice = Reg.ProductPrice;
+                    EL.ProductQuantity = Reg.ProductQuantity;
+                    EL.BuyerAddress = Reg.BuyerAddress;
+                    EL.City = Reg.City;
+                    EL.BuyerState = Reg.BuyerState;
+                    EL.Pincode = Reg.Pincode;
+                    EL.Country = Reg.Country;
+                    EL.Mobile = Reg.Mobile;
+                    EL.Email = Reg.Email;
+                    EL.DeliverCharge = Reg.DeliverCharge;
+                    DB.OrderTables.Add(EL);
+                    DB.SaveChanges();
+                    return new Response
+                    { Status = "Success", Message = "Order successfully placed." };
+                }
+            }
+            catch (Exception)
+            {
+                return new Response
+                { Status = "Failure", Message = "Order already placed." };
+                //throw;
+            }
+            return new Response
+            { Status = "Error", Message = "Invalid Data." };
+        }
+    }
+
+}
